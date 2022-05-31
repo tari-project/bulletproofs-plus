c4dffb2dfb379aed00c380060fa858d227fc1c9f
stringhandler <mikethetike@tari.com>
2022-05-31 11:10:57 +0200

    ci: add test coverage script (#11)

---
+++ test_coverage.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Prerequisites
+# 1. You need LLVM-COV tools:
+# $ rustup component add llvm-tools-preview
+# 2. and Rust wrappers for llvm-cov:
+# $ cargo install cargo-binutils
+# 3. The rust name demangler
+# $ cargo install rustfilt
+# 4. jq
+# 5. genhtml
+# $ sudo apt install lcov
+
+RUSTFLAGS="-Z instrument-coverage"
+LLVM_PROFILE_FILE="./cov_raw/bulletproofs-plus-%m.profraw"
+
+get_binaries() {
+  files=$( RUSTFLAGS=$RUSTFLAGS cargo test --tests --no-run --message-format=json \
+              | jq -r "select(.profile.test == true) | .filenames[]" \
+              | grep -v dSYM - \
+        );
+  files=("${files[@]/#/-object }")
+}
+
+get_binaries
+
+# Remove old coverage files
+rm cov_raw/*profraw cov_raw/bulletproofs-plus.profdata cov_raw/bulletproofs-plus.lcov cov_raw/bulletproofs-plus.txt
+
+RUSTFLAGS=$RUSTFLAGS LLVM_PROFILE_FILE=$LLVM_PROFILE_FILE cargo test --tests
+
+cargo profdata -- \
+  merge -sparse ./cov_raw/bulletproofs-plus-*.profraw -o ./cov_raw/bulletproofs-plus.profdata
+
+cargo cov -- \
+  export \
+    --Xdemangler=rustfilt \
+    --format=lcov \
+    --show-branch-summary \
+    --show-instantiation-summary \
+    --show-region-summary \
+    --ignore-filename-regex='/.cargo/registry' \
+    --ignore-filename-regex="^/rustc" \
+    --instr-profile=cov_raw/bulletproofs-plus.profdata \
+    $files \
+    > cov_raw/bulletproofs-plus.lcov
+
+cargo cov -- \
+  show \
+    --Xdemangler=rustfilt \
+    --show-branch-summary \
+    --show-instantiation-summary \
+    --show-region-summary \
+    --ignore-filename-regex='/.cargo/registry' \
+    --ignore-filename-regex="^/rustc" \
+    --instr-profile=cov_raw/bulletproofs-plus.profdata \
+    $files \
+    > cov_raw/bulletproofs-plus.txt
+
+if [ -z ${SKIP_HTML+x} ]; then
+  genhtml -o coverage_report cov_raw/bulletproofs-plus.lcov
+else
+  echo "Skipping html generation"
+fi
+# open coverage_report/src/index.html
