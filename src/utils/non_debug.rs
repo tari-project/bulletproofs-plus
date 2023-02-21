ebae2f904f07e00a034cbc796c58aa112c5a2144
SW van Heerden <swvheerden@gmail.com>
2023-02-21 13:04:10 +0200

    non default

--- src/utils/non_debug.rs
+++ src/utils/non_debug.rs
@@ -5,10 +5,10 @@
 
 use core::fmt;
 
-use derive_more::{AsMut, AsRef, Deref, DerefMut, From};
+use derive_more::{ Deref, DerefMut, From};
 
 /// A struct to add 'Debug' functionality to other struct members that do not implement 'Debug'
-#[derive(Copy, Clone, From, Deref, DerefMut, AsRef, AsMut)]
+#[derive(Copy, Clone, From, Deref, DerefMut)]
 pub struct NonDebug<T> {
     inner: T,
 }
