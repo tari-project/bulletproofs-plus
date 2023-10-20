a81b105b4bc0a994bd68336d2b5720a98fbc0c7b
Aaron Feickert <66188213+AaronFeickert@users.noreply.github.com>
2023-10-20 09:39:55 -0500

    test: basic fuzzing (#79)
    Adds basic deserialization fuzzing to the library to avoid unexpected
    panics. Updates documentation.
    
    Because this is not part of CI, it should be tested manually. To do so,
    [install](https://github.com/rust-fuzz/cargo-fuzz) `cargo-fuzz` and run
    it with the nightly toolchain until you get bored: `cargo +nightly fuzz
    run proofs`
    
    Closes #78.

---
+++ fuzz/fuzz_targets/proofs.rs
@@ -0,0 +1,15 @@
+// Copyright 2023 The Tari Project
+// SPDX-License-Identifier: BSD-3-Clause
+
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use tari_bulletproofs_plus::ristretto::RistrettoRangeProof;
+
+// Test basic deserialization and canonical serialization
+fuzz_target!(|data: &[u8]| {
+	// If deserialization succeeds, serialization should be canonical
+	if let Ok(proof) = RistrettoRangeProof::from_bytes(data) {
+		assert_eq!(&proof.to_bytes(), data);
+	}
+});
